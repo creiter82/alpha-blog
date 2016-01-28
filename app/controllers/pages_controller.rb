4c8226e76fa3481576f8460fd80cd12a84d83762
Cameron Reiter <cameron@intrinsicbusinessconsulting.comgit configexitgit config --global user.email cameron@intrinsicbusinessconsulting.com>
2016-01-28 03:10:29 +0000

    complete authentication system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
   def home
-    
+    # if logged in you'll be redirected to your articles page
+    # else if not logged in you'll see the homepage with link to signup
+    redirect_to articles_path if logged_in?
   end
   
   def about
